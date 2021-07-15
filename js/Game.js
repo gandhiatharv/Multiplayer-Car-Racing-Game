fc5a17cc981a1aed2400bc72e6ccea1d8cbdf693
gandhiatharv <77360139+gandhiatharv@users.noreply.github.com>
2021-07-15 14:49:58 -0400

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -28,9 +28,13 @@ class Game {
       form.display();
     }
     car1 = createSprite(100, 200);
+    car1.addImage(car1img);
     car2 = createSprite(300, 200);
+    car2.addImage(car2img);
     car3 = createSprite(500, 200);
+    car3.addImage(car3img);
     car4 = createSprite(700, 200);
+    car4.addImage(car4img);
     cars = [car1, car2, car3, car4];
 
   }
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
+      background(ground);
+      image(track, 0, -displayHeight*4, displayWidth, displayHeight*5); 
       var index = 0;
-      var x = 0;
+      var x = 225;
       var y;
       //var display_position = 130;
       for(var plr in allPlayers){
         index = index + 1;
-        x = x + 200;
+        x = x + 275;
         y = displayHeight-allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
@@ -61,9 +67,17 @@ class Game {
     }
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
+
       player.distance +=50
       player.update();
     }
+    if(player.distance > 5250){
+      gameState = 2;
+      //swal({ title: `Awesome!${"\n"}Rank${"\n"}${player.rank}`, text: "You reached the finish line successfully", imageUrl: "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png", imageSize: "100x100", confirmButtonText: "Ok", });
+    }
     drawSprites();
+  }
+  end(){
+    console.log("Game Ended");
   }
 }
