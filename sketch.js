73828d798703e15d3566f7bf7bdd9e494ec6d9e3
gandhiatharv <77360139+gandhiatharv@users.noreply.github.com>
2021-07-20 18:07:44 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var distance = 0;
 var database;
 var car1, car2, car3, car4, cars;
 var car1img, car2img, car3img, car4img, track, ground, lobbysound, playsound, endsound;
+var obstacle1, obstacle2, obstacle3;
 
 var form, player, game;
 
@@ -20,6 +21,9 @@ function preload(){
   lobbysound = loadSound("sound/lobbysound.mp3");
   playsound = loadSound("sound/gamesound.mp3");
   endsound = loadSound("sound/endsound.mp3");
+  obstacle1 = loadImage("images/obstacle1.png");
+  obstacle2 = loadImage("images/obstacle2.png");
+  obstacle3 = loadImage("images/obstacle3.png");
 }
 
 function setup(){
@@ -39,11 +43,12 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+    spawnObstacles();
     lobbysound.stop();
     endsound.stop();
-    playsound.play();
+    //playsound.play();
     if(frameCount%7020 === 0){
-      playsound.play();
+      //playsound.play();
       }
   }
 
@@ -51,15 +56,32 @@ function draw(){
     game.end();
     lobbysound.stop();
     playsound.stop();
-    endsound.play();
+    //endsound.play();
   }
 
   if(gameState === 0){
 playsound.stop();
 endsound.stop();
-lobbysound.play();
+//lobbysound.play();
 if(frameCount%6870 === 0){
-  lobbysound.play();
+  //lobbysound.play();
   }
 }
+}
+
+function spawnObstacles(){
+    if(frameCount%60 === 0 && gameState === 1){
+      obstacle = createSprite(Math.round(random(200, width-150)), 0, 200, 200);
+      obstacle.lifetime = 1000;
+      obstacle.velocityY = 7;
+      obstacle.scale = 0.1;
+      var rand = Math.round(random(1, 3));
+      if(rand === 1){
+        obstacle.addImage(obstacle1);
+      } else if(rand === 2){
+        obstacle.addImage(obstacle2);
+      } else{
+        obstacle.addImage(obstacle3);
+      }
+    }
 }
