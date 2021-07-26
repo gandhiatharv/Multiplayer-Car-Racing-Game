f22068ce2854a87853f809a28333741624eb92bb
gandhiatharv <77360139+gandhiatharv@users.noreply.github.com>
2021-07-26 18:01:05 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,13 +4,14 @@ var gameState = 0;
 var playerCount;
 var allPlayers;
 var distance = 0;
+var distance1 = 0;
 var tries = 2;
 var tries2 = 2;
 var database;
 var car1, car2, car3, car4, cars;
 var car1img, car2img, car3img, car4img, ground, lobbysound, playsound, endsound;
 var obstacle1, obstacle2, obstacle3;
-var track
+var track, obstacle;
 
 var form, player, game;
 
@@ -61,27 +62,9 @@ if(tries2 === 2 && playerCount < 4){
   if(gameState === 1){
     clear();
     game.play();
-    spawnObstacles();
   }
 
   if(gameState === 2){
     game.end();
   }
-}
-
-function spawnObstacles(){
-    if(frameCount%30 === 0 && gameState === 1){
-      obstacle = createSprite(Math.round(random(displayWidth/4.4, displayWidth/1.29)), 0, 200, 200);
-      obstacle.lifetime = 300;
-      obstacle.velocityY = 7;
-      obstacle.scale = 0.1;
-      var rand = Math.round(random(1, 3));
-      if(rand === 1){
-        obstacle.addImage(obstacle1);
-      } else if(rand === 2){
-        obstacle.addImage(obstacle2);
-      } else{
-        obstacle.addImage(obstacle3);
-      }
-    }
 }
